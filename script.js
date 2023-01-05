54f2ec3d32d472e845ee4702f292c90b0e463ec8
santanagabi <gaboliveirasantana1@gmail.com>
2023-01-04 21:38:12 -0300

    Working with classes

--- script.js
+++ script.js
@@ -5,16 +5,23 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', function () {
+// Função para abrir o Modal
+const openModal = function () {
 
-    console.log('Button clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
-btnCloseModal.addEventListener('click', function () {
+// Função para fechar o Modal
+const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-});
+};
 
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
