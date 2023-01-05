9f377da5b0d0ba712a7882e628831fd0e8a26601
santanagabi <gaboliveirasantana1@gmail.com>
2023-01-04 22:19:14 -0300

    Handling and "Esc"  keypress event

--- script.js
+++ script.js
@@ -5,15 +5,11 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-// Função para abrir o Modal
 const openModal = function () {
-
-  console.log('Button clicked');
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 
-// Função para fechar o Modal
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
@@ -23,5 +19,9 @@ for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
-
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden'))
+    closeModal();
+});
